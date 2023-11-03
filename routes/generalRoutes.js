a55357caa47368def3b8187318d38ff489592c04
Raul <raulreyesbatalla@outlook.es>
2023-11-03 13:29:00 -0600

    arreglando problemas con el index (servidor)

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -1,5 +1,7 @@
-import { Express } from "express";
+//importar la libreria de Express para activar la comunicacionvia protocolo HTTP
+import express from 'express'
 
+// Instancuamos el modulo express de la libreria para definir el servidor que atendera las peticiones 
 const router = express.Router();
 
 router.get('/', (request, response) => response.render("./layout/index.pug", {page:"Home"}))
