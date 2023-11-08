8f8e52a84e2639e664674aaa93b6dc345f7c744a
Raul <raulreyesbatalla@outlook.es>
2023-11-08 10:00:56 -0600

    arreglando identacion

--- models/User.js
+++ models/User.js
@@ -3,44 +3,45 @@ import db from '../config/db.js';
 import bcrypt from "bcrypt";
 
 
-const TbbUser= db.define("tbb_users",
-{
-    name:{
-        type: DataTypes.STRING(255),
-        // indica que es obligatorio 
-        allowNull: false
+const TbbUser = db.define("tbb_users",
+  {
+    name: {
+      type: DataTypes.STRING(255),
+      // indica que es obligatorio 
+      allowNull: false
     },
-    email:{
-        type: DataTypes.STRING(255),
-        allowNull:false,
-        unique:true
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true
     },
-    password:{
-        type: DataTypes.STRING,
-        allowNull:false,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     token: {
-        type: DataTypes.STRING,
-         unique: true   
-        },// token: al no definir el que es obligatorio, lo toma como opcional 
-        verified: {
-        type:DataTypes.BOOLEAN, 
-        defaultValue: true
-    }},
-    {
-        hooks:{
-            beforeCreate: async(User)=>{
-                const salt = await bcrypt.genSalt(10);
-                User.password=await bcrypt.hash(User.password, salt) ;
-                
-            }
-
-        }
+      type: DataTypes.STRING,
+      unique: true
+    },// token: al no definir el que es obligatorio, lo toma como opcional 
+    verified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
     }
+  },
+  {
+    hooks: {
+      beforeCreate: async (User) => {
+        const salt = await bcrypt.genSalt(10);
+        User.password = await bcrypt.hash(User.password, salt);
+
+      }
+
+    }
+  }
 );
- // compara contraseñas por medio del hasing 
-User.prototype.verifiedPassword=(password)=>{
-return bcrypt.compareSync(password,this.password);
+// compara contraseñas por medio del hasing 
+User.prototype.verifiedPassword = (password) => {
+  return bcrypt.compareSync(password, this.password);
 }
- 
+
 export default TbbUserUser
