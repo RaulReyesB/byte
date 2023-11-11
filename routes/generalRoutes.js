2066a03dc5c08dc7ed3da549aa6541a4afe8e487
Raul <raulreyesbatalla@outlook.es>
2023-11-11 02:34:53 -0600

    Cambiando la ruta de /Inicio a /(default) ademas añadiendo la pagina principal y ya esta casi acabada, agregando fotografias y nuevo logo

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -4,7 +4,6 @@ import express, { response } from 'express'
 // Instancuamos el modulo express de la libreria para definir el servidor que atendera las peticiones 
 const router = express.Router();
 
-router.get('/', (request, response) => response.render("./layout/index.pug", {page:"Home"}))
-router.get("/Inicio", (request, response) => response.render("./auth/Inicio" ,{page: "Inicio", showHeader:true}))
+router.get('/', (request, response) => response.render("./auth/Inicio", {page:"Inicio",showHeader:true, showFooter:true}))
 
 export default router;
