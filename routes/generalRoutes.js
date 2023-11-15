31e333da88093ef1e9642539f4159170885ab850
Raul <raulreyesbatalla@outlook.es>
2023-11-15 12:02:05 -0600

    agregando viajes

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -5,5 +5,6 @@ import express, { response } from 'express'
 const router = express.Router();
 
 router.get('/', (request, response) => response.render("./auth/Inicio", {page:"Inicio",showHeader:true, showFooter:true}))
+router.get('/viajes', (request, response) => response.render("./auth/viajes", {page:"Viajes",showHeader:true, showFooter:true}))
 
 export default router;
