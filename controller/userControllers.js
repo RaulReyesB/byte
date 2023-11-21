d0d495ae1b8073297208b7eeb9185416f44f0c99
ZaS-ex <a.zacek.gc@gmail.com>
2023-11-21 13:35:02 -0600

    Modificacion de los controladores de usuario

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -30,11 +30,11 @@ const tiket = (request, response) => {
 
 
 const insertUser = async (request, response) => {
-  await check("name").notEmpty().withMessage("This field is REQUIRED: NAME").run(request)
+  await check("name").notEmpty().withMessage("Este campo es OBLIGATORIO: NOMBRE").isLength({min: 4}).withMessage("El nombre debe contener 4 caracteres como minimo").isLength({max: 15}).withMessage("El nombre debe contener 15 caracteres maximo").run(request)
 
-  await check("email").notEmpty().withMessage("This field is REQUIRED: EMAIL").run(request)
+  await check("email").notEmpty().withMessage("Este campo es OBLIGATORIO: EMAIL").isEmail().withMessage("El valor debe estar en formato User@domain.ext").run(request)
 
-  await check("password").notEmpty().withMessage("This field is REQUIRED: PASSWORD").run(request)
+  await check("password").notEmpty().withMessage("Este campo es OBLIGATORIO: PASSWORD").isLength({min: 8}).withMessage("la contraseña debe contener al menos 8 caracteres").isLength({max: 20}).withMessage("Password must contain less than 20 characters").equals(request.body.cpassword).withMessage("Both password must be the same").run(request)
 }
 
 export {
