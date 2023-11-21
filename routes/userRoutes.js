6572313b795f886293f61a8638c48e84375e18b5
ZaS-ex <a.zacek.gc@gmail.com>
2023-11-21 13:50:45 -0600

    Modificaion de las rutas de usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express, {Router} from 'express'
-import { formularioLogin,formularioOlvidoContra,formularioRegistro, ticket } from '../controller/userControllers.js';
+import { formularioLogin,formularioOlvidoContra,formularioRegistro, tiket } from '../controller/userControllers.js';
 
 const router = express.Router();
 
 router.get("/login", formularioLogin);
 router.get("/register", formularioRegistro);
 router.get("/forgot-password",formularioOlvidoContra);
-router.get("/tiket", ticket)
+router.get("/tiket", tiket)
 export default router;
