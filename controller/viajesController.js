7a2b5bedad21af0af2ee2127addb23ad124c9c99
Raul <raulreyesbatalla@outlook.es>
2023-12-10 05:45:33 -0600

    me muero de sueño

--- controller/viajesController.js
+++ controller/viajesController.js
@@ -1,8 +1,21 @@
+import Viaje from '../models/Viaje.js';
 import { request, response } from "express";
 import { check } from "express-validator";
 import { validationResult } from "express-validator";
 import Result from "postcss/lib/result";
 
+const obtenerDetallesDelViajeDesdeBD = async (viajeId) => {
+  try {
+    // Utiliza tu modelo y las asociaciones definidas para obtener los detalles del viaje
+    const detallesViaje = await Viaje.findByPk(viajeId, { include: 'horarios' });
+
+    // Aquí podrías realizar cualquier otro procesamiento necesario
+
+    return detallesViaje;
+  } catch (error) {
+    throw new Error('Error al obtener detalles del viaje desde la base de datos');
+  }
+};
 
 const viajes = (request, response) => {
   response.render('auth/viajes', {
@@ -29,8 +42,58 @@ const precios = (request, response) => {
   })
 }
 
+
+const detalleViaje = async (request, response) => {
+  try {
+    const viajeId = request.params.id;
+
+    // Obtener datos del viaje según el ID utilizando el modelo Viaje
+    const viaje = await Viaje.findByPk(viajeId, { include: 'horarios' });
+
+    // Renderizar la vista de detalles de asientos de ida y pasar los datos del viaje
+    response.render('./viajes/detalle.pug', {
+      viaje,
+      horarios: viaje.horarios,  // Asegúrate de tener una propiedad horarios en tu modelo Viaje
+      pagina: "Horarios",
+      showHeader: true,
+      showFooter: true,
+      // Otros datos que quieras pasar a la vista
+    });
+  } catch (error) {
+    console.error(error);
+    response.status(500).render('error', {
+      errorMessage: 'Error interno del servidor',
+    });
+  }
+};
+
+const obtenerDetallesViaje = async (request, response) => {
+  try {
+    const viajeId = request.params.id;
+    
+    // Aquí obtienes la información del viaje, incluyendo horarios si es necesario
+    const viaje = await Viaje.findByPk(viajeId, { include: 'horarios' });
+
+    // Renderiza la vista de detallesViaje y pasa los datos necesarios
+    response.render('./viajes/detallesViaje.pug', {
+      viaje,
+      pagina: "Detalles del Viaje",
+      showHeader: true,
+      showFooter: true,
+      // Otros datos que quieras pasar a la vista
+    });
+  } catch (error) {
+    console.error(error);
+    response.status(500).render('error', {
+      errorMessage: 'Error interno del servidor',
+    });
+  }
+};
+
 export {
   viajes,
   horarios,
-  precios
+  precios,
+  detalleViaje,
+  obtenerDetallesViaje
 } 
--- routes/viajesRoutes.js
+++ routes/viajesRoutes.js
@@ -1,38 +1,18 @@
 import express, { Router, request, response } from 'express'
-import { viajes, horarios, precios } from '../controller/viajesController.js';
+import { viajes, horarios, precios, detalleViaje, obtenerDetallesViaje  } from '../controller/viajesController.js';
 import protegerRuta from '../middleware/protegerRutas.js';
-import Viaje from '../models/Viaje.js';
-import Horario from '../models/Horario.js';
+
 const router = express.Router();
 
 router.get("/", viajes);
 router.get('/horarios', horarios)
 router.get('/PreciosIda', precios)
 
-// En tu controlador
-router.get('/detalle/:id', async (request, response) => {
-  try {
-    const viajeId = request.params.id;
+//Detalles del viaje que se seleciona por medio del id
+router.get('/detalle/:id', detalleViaje);
 
-    // Obtener datos del viaje según el ID utilizando el modelo Viaje
-    const viaje = await Viaje.findByPk(viajeId, { include: 'horarios' });
 
-    // Renderizar la vista de detalles de asientos de ida y pasar los datos del viaje
-    response.render('./viajes/detalle.pug', {
-      viaje,
-      horarios: viaje.horarios,  // Asegúrate de tener una propiedad horarios en tu modelo Viaje
-      pagina: "Horarios",
-      showHeader: true,
-      showFooter: true,
-      // Otros datos que quieras pasar a la vista
-    });
-  } catch (error) {
-    console.error(error);
-    response.status(500).render('error', {
-      errorMessage: 'Error interno del servidor',
-    });
-  }
-});
+router.get('/detallesViaje/:id', obtenerDetallesViaje);
 
 
 export default router;
